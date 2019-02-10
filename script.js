ca8c6a0315c89caf8f9bdfcce98b7074d66f432e
Oscar <oscara@csedge.org>
2019-02-09 23:07:16 -0800

    23e

--- script.js
+++ script.js
@@ -1,16 +1,42 @@
 $(document).ready(function(){
 // all jQuery methods go here...
 
+// starter dance
+$("#runningman-img").hover(enter, leave);
+function enter(){
+  this.src = "images/runningman-giphy.gif"
+}
+function leave() {
+  this.src = "images/runningman-icon.png"
+}
 
+// Take the L
+$("#taketheL-img").hover(enter1, leave1);
+function enter1(){
+  this.src = "images/taketheL-giphy.gif"
 
+}
+function leave1() {
+  this.src = "images/taketheL-icon.png"
+}
 
+// Electroshuffle
+$("#electroshuffle-img").hover(enter2, leave2);
+function enter2(){
+  this.src = "images/electroshuffle-giphy.gif"
+}
+function leave2() {
+  this.src = "images/electroshuffle-icon.png"
+}
 
-
-
-
-
-
-
+// Bestmates
+$("#bestmates-img").hover(enter3, leave3);
+function enter3(){
+  this.src = "images/bestmates-giphy.gif"
+}
+function leave3() {
+  this.src = "images/bestmates-icon.png"
+}
 
 
 
